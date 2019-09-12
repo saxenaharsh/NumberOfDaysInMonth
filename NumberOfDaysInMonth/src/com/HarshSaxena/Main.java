b66f50a2c4ea53f7986dfcdcb34eddb8a0d8a8a2
saxenaharsh <harsh.saxena@gmail.com>
2019-09-12 15:33:39 +0530

    Add files via upload

---
+++ NumberOfDaysInMonth/src/com/HarshSaxena/Main.java
@@ -0,0 +1,42 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //isLeapYear(2000);
+        getDaysInMonth(2, 2018);
+
+    }
+    public static boolean isLeapYear(int year){
+        if((year < 1) || (year > 9999)){
+            return false;
+        }else{
+            if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+                return true;
+            }
+            return false;
+        }
+    }
+    public static int getDaysInMonth(int month, int year){
+        if(month < 1 || month > 12 || year < 1 || year > 9999){
+            return -1;
+        }else{
+            switch(month){
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    return 31;
+                case 2:
+                    return  (isLeapYear(year) ? 29 : 28);
+                case 4: case 6: case 9: case 11:
+                    return 30;
+                default:
+                    return -1;
+
+
+
+            }
+        }
+    }
+
+
+}
